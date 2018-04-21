02e8e37f576aea60457759c6705ec43f9bcaef7e
Lilun <iamlilun@taijun.net>
2018-04-21 18:26:48 +0800

    add todolist

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<<script>
+import TodoInput from './TodoInput.vue';
+import TodoItem from './TodoItem.vue';
+
+export default{
+  components:{
+    TodoInput,
+    TodoItem, 
+  },
+  data(){
+    return {
+      todos: [],
+    };
+  },
+  methods:{
+    addTodo(text){
+      this.todos.push(text);
+    }
+  }
+};
+</script>
+
+<template>
+  <div>
+    <TodoInput @submit="addTodo" />
+    <ul>
+      <TodoItem v-for="todo in todos" :key='todo' :data="todo" />
+    </ul>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ src/TodoInput.vue
@@ -0,0 +1,24 @@
+<template>
+    <form @submit.prevent="submit">
+        <input v-model="txet" />
+        <button type="submit">Submit</button>
+    </form>
+</template>
+
+<<script>
+export default {
+  data(){
+      return {
+          txet: "",
+      }
+  },
+  methods:{
+      submit(){
+        console.log('fuckV-model', this.text);
+        this.$emit('submit', this.text);
+        this.text = '';
+      },
+  }
+}
+</script>
+
